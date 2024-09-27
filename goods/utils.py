afd26ade488315982028e7482b61111e6e7783ac
clazy <fu1tu4re>
2024-09-27 10:18:40 +0300

    Full text seacrh well done

--- goods/utils.py
+++ goods/utils.py
@@ -1,6 +1,6 @@
 from django.db.models import Q
 from goods.models import Products, Categories
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
 
 
 def q_search(search):
@@ -9,7 +9,28 @@ def q_search(search):
 
     vector = SearchVector("name", "description")
     query = SearchQuery(search)
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+
+    result = Products.objects.annotate(rank=SearchRank(
+        vector, query)).filter(rank__gt=0).order_by("-rank")
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+
+    return result
 
     # keywords = [word for word in search.split() if len(word) > 2]
     # q_objects = Q()
